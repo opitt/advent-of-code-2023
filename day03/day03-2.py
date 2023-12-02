bf9524bb114eae061b090e1255d70491b84692d7
opitt <63d64c2bc2b1cb6b346f0a1d@bots.bitbucket.org>
2023-12-02 13:22:08 +0100

    Prepare for day 3

---
+++ day03/day03-2.py
@@ -0,0 +1,26 @@
+# https://adventofcode.com/2023/day/3
+from doctest import REPORT_ONLY_FIRST_FAILURE
+import os
+
+def solve(input_lines):
+    resut=0
+    ...
+    return result
+
+
+def main(test):
+    # READ INPUT FILE
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(script_path, "test.txt" if test else "input.txt"), encoding="utf-8") as input:
+        lines = input.readlines()
+    lines = list(map(str.strip, lines))
+
+    result = solve(lines)
+    print(
+        f"The result is {result}."
+    )
+    # ???
+
+
+main(test=True)
+#main(test=False)
