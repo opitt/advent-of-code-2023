6e8f5064e6130dfa95060d47e00713ba6906c2b0
opitt <63d64c2bc2b1cb6b346f0a1d@bots.bitbucket.org>
2023-12-03 09:09:53 +0100

    Add comments

--- day03/day03-1.py
+++ day03/day03-1.py
@@ -3,45 +3,51 @@ import os
 import re
 from collections import namedtuple
 
+
 def solve(input_lines):
-    lines = ["."*len(input_lines)]
+    # extend the playing field to simplify indexing
+    lines = ["." * len(input_lines)]
     lines.extend(input_lines)
     lines.append(lines[0])
-    lines = [f".{line}." for line in lines ]
+    lines = [f".{line}." for line in lines]
 
+    # find all numbers
     Partno = namedtuple("Partno", "value y x1 x2")
-    
-    numbers=[]
+    numbers = []
     for y, line in enumerate(lines):
-        matches=re.finditer("(\d+)",line)
+        matches = re.finditer("(\d+)", line)
         if matches:
-            numbers.extend([Partno(int(m.group()), y, m.start(), m.end()-1) for m in matches])
+            numbers.extend(
+                [Partno(int(m.group()), y, m.start(), m.end() - 1) for m in matches]
+            )
 
-    result=0
+    # find all numbers, except the ones, that only touch a "."
+    result = 0
     for partno in numbers:
-        adjacent=lines[partno.y-1][partno.x1-1:partno.x2+2]
-        adjacent+=lines[partno.y][partno.x1-1]
-        adjacent+=lines[partno.y][partno.x2+1]
-        adjacent+=lines[partno.y+1][partno.x1-1:partno.x2+2]
-        is_not_part = re.fullmatch("[\.]+",adjacent)
+        adjacent = lines[partno.y - 1][partno.x1 - 1 : partno.x2 + 2]
+        adjacent += lines[partno.y][partno.x1 - 1]
+        adjacent += lines[partno.y][partno.x2 + 1]
+        adjacent += lines[partno.y + 1][partno.x1 - 1 : partno.x2 + 2]
+        is_not_part = re.fullmatch("[\.]+", adjacent)
         if not is_not_part:
-            result+=partno.value
+            result += partno.value
 
     return result
+
 
 def main(test):
     # READ INPUT FILE
     script_path = os.path.dirname(os.path.realpath(__file__))
-    with open(os.path.join(script_path, "test.txt" if test else "input.txt"), encoding="utf-8") as input:
+    with open(
+        os.path.join(script_path, "test.txt" if test else "input.txt"), encoding="utf-8"
+    ) as input:
         lines = input.readlines()
     lines = list(map(str.strip, lines))
 
     result = solve(lines)
-    print(
-        f"The result is {result}."
-    )
-    # ???
+    print(f"The result is {result}.")
+    # 540212
 
 
-#main(test=True)
+# main(test=True)
 main(test=False)
