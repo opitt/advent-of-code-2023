34658da483fca1fc5697e8c6353d1bb7746aff7f
opitt <63d64c2bc2b1cb6b346f0a1d@bots.bitbucket.org>
2023-12-05 07:36:38 +0100

    Part 1 solved

---
+++ day05/day05-1.py
@@ -0,0 +1,51 @@
+# https://adventofcode.com/2023/day/5
+import os
+import re
+from collections import namedtuple
+
+Fromto = namedtuple("Fromto", "dest_min dest_max src_min src_max")
+
+
+def solve(lines):
+
+    def find_location(seed, almanac):
+        thing = seed
+        for almanac_line in almanac:
+            for fromto in almanac_line:
+                if fromto.src_min <= thing <= fromto.src_max:
+                    thing = fromto.dest_min + thing - fromto.src_min
+                    break
+        return thing
+
+    seeds = list(map(int, lines[0].split()[1:]))
+    mapping = []
+    for line in lines[2:]:
+        if line == "":
+            continue
+        if re.match("[a-z].+", line):
+            mapping.append([])
+        else:
+            dest, src, length = map(int, line.split())
+            mapping[-1].append(Fromto(dest, dest + length - 1, src, src + length - 1))
+
+    locations = [find_location(seed, mapping) for seed in seeds]
+
+    return min(locations)
+
+
+def main(test):
+    # READ INPUT FILE
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    with open(
+        os.path.join(script_path, "test.txt" if test else "input.txt"), encoding="utf-8"
+    ) as input:
+        lines = input.readlines()
+    lines = list(map(str.strip, lines))
+
+    result = solve(lines)
+    print(f"The result is {result}.")
+    # 993500720
+
+
+# main(test=True)
+main(test=False)
