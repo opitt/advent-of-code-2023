cec3c497d19bb8ffdc17b4cc1ef697abc962d919
opitt <23746293+opitt@users.noreply.github.com>
2023-12-12 17:34:44 +0100

    Solved part 2. With slicing.

--- day05/day05-2.py
+++ day05/day05-2.py
@@ -19,21 +19,26 @@ def solve(lines):
                         seed_a = fromto.a
                     else:
                         result.append((seed_a, seed_b))
+                        seed_a = None
                         break
                 if seed_a <= fromto.b:
                     if seed_b <= fromto.b:
                         result.append(
                             (
-                                seed_a,
-                                seed_b,
+                                fromto.to_a + (seed_a - fromto.a), #seed_a
+                                fromto.to_a + (seed_b - fromto.a) #seed_b,
                             )
                         )
+                        seed_a = None
                         break
                     else:
                         result.append(
-                            (seed_a, fromto.to_b)
+                            (fromto.to_a + (seed_a - fromto.a), #seed_a
+                             fromto.to_b)
                         )
                         seed_a = fromto.b + 1
+            if seed_a != None:
+                result.append((seed_a, seed_b))
             print(result)
 
         return deepcopy(result)
@@ -86,5 +91,5 @@ def main(test):
     #
 
 
-main(test=True)
-# main(test=False)
+#main(test=True)
+main(test=False)
